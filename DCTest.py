38f952d640241ce6f2082f59ab90c5a76f053ffd
evargh <varghese.evan@gmail.com>
2018-09-23 13:52:26 -0500

    Edited test

--- DCTest.py
+++ DCTest.py
@@ -26,33 +26,33 @@ myMotor.run(Adafruit_MotorHAT.FORWARD);
 myMotor.run(Adafruit_MotorHAT.RELEASE);
 
 
-while (True):
-    print("Forward! ")
-    myMotor.run(Adafruit_MotorHAT.FORWARD)
+# while (True):
+    # print("Forward! ")
+    # myMotor.run(Adafruit_MotorHAT.FORWARD)
 
-    print("\tSpeed up...")
-    for i in range(255):
-        myMotor.setSpeed(i)
-        time.sleep(0.01)
+    # print("\tSpeed up...")
+    # for i in range(255):
+        # myMotor.setSpeed(i)
+        # time.sleep(0.01)
 
-    print("\tSlow down...")
-    for i in reversed(range(255)):
-        myMotor.setSpeed(i)
-        time.sleep(0.01)
+    # print("\tSlow down...")
+    # for i in reversed(range(255)):
+        # myMotor.setSpeed(i)
+        # time.sleep(0.01)
 
-    print("Backward! ")
-    myMotor.run(Adafruit_MotorHAT.BACKWARD)
+    # print("Backward! ")
+    # myMotor.run(Adafruit_MotorHAT.BACKWARD)
 
-    print("\tSpeed up...")
-    for i in range(255):
-        myMotor.setSpeed(i)
-        time.sleep(0.01)
+    # print("\tSpeed up...")
+    # for i in range(255):
+        # myMotor.setSpeed(i)
+        # time.sleep(0.01)
 
-    print("\tSlow down...")
-    for i in reversed(range(255)):
-        myMotor.setSpeed(i)
-        time.sleep(0.01)
+    # print("\tSlow down...")
+    # for i in reversed(range(255)):
+        # myMotor.setSpeed(i)
+        # time.sleep(0.01)
 
-    print("Release")
-    myMotor.run(Adafruit_MotorHAT.RELEASE)
-    time.sleep(1.0)
+    # print("Release")
+    # myMotor.run(Adafruit_MotorHAT.RELEASE)
+    # time.sleep(1.0)
