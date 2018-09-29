f4cf51076c2ae375db3a21d47e5695d7d92234de
evargh <varghese.evan@gmail.com>
2018-09-29 13:16:05 -0500

    Now usable with two probes

--- GrillHeater.py
+++ GrillHeater.py
@@ -33,21 +33,24 @@ pi = pigpio.pi()
 if not pi.connected:
    exit(0)
 
-sensor = pi.spi_open(0, 1000000, 0) # CE0 on auxiliary SPI
+sensorMeat = pi.spi_open(0, 1000000, 0) # CE0 on main SPI
+sensorBot = pi.spi_open(1, 1000000, 0) # CE1 on main SPI
 
 stop = time.time() + 600
 
 while time.time() < stop:
    myMotor.run(Adafruit_MotorHAT.FORWARD)
    c, d = pi.spi_read(sensor, 2)
-   if c == 2:
+   e, f = pi.spi_read(sensorBot, 2)
+   if c == 2 and e == 2:
       word = (d[0]<<8) | d[1]
-      if (word & 0x8006) == 0: # Bits 15, 2, and 1 should be zero.
-         t = 9.0*(word >> 3)/20.0 + 32.0
+      if (word & 0x8006) == 0:if (wordMeat & 0x8006) == 0 and (wordBot & 0x8006) == 0: # Bits 15, 2, and 1 should be zero.
+         t = 9*(wordMeat >> 3)/20.0 + 32.0
+         u = 9*(wordBot >> 3)/20.0 + 32.0
          yeet = "{:.2f}".format(t)
          speed = int(t) * 3 - 20
          myMotor.setSpeed(speed)
-         print(yeet + ' '+ str(speed))
+         print(yeet + ' ' + + "{:.2f}".format(u) + ' '+ str(speed))
       else:
          print("bad reading {:b}".format(word))
    time.sleep(0.25) # Don't try to read more often than 4 times a second.
