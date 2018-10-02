b15eedf5d22cbd103092987b9a7d32d431fcc839
evargh <varghese.evan@gmail.com>
2018-10-02 18:04:05 -0500

    finalizing things

--- GrillHeater.py
+++ GrillHeater.py
@@ -53,38 +53,39 @@ def index():
 if __name__ == "__main__":
     # hosts the site locally
     app.run('0.0.0.0', port=5000)
+    while True:
+       myMotor.run(Adafruit_MotorHAT.FORWARD)
+       c, d = pi.spi_read(sensorMeat, 2)
+       e, f = pi.spi_read(sensorBot, 2)
+       if c == 2 and e == 2:
+          wordMeat = (d[0]<<8) | d[1]
+          wordBot = (f[0]<<8) | f[1]
+          if (wordMeat & 0x8006) == 0 and (wordBot & 0x8006) == 0: # Bits 15, 2, and 1 should be zero.
+             t = 9*(wordMeat >> 3)/20.0 + 32.0
+             u = 9*(wordBot >> 3)/20.0 + 32.0
+             delta = t - target
+             if(abs(time.time() - timeStat - 10) < 1):
+                 timeStat = time.time()
+                 if delta < -20:
+                     if motorState == False:
+                         motorState = True
+                         myMotor.setSpeed(100)
+                     else:
+                         motorState == False
+                         myMotor.setSpeed(0)
+                 elif delta < 0:
+                     if motorState == False:
+                         motorState = True
+                         myMotor.setSpeed(60)
+                     else:
+                         motorState == False
+                         myMotor.setSpeed(0)
+             print("{:.2f}".format(t) + ' ' + "{:.2f}".format(u) + ' ' + "{:.2f}".format(delta))
+          else:
+             print("bad reading {:b}".format(word))
+       time.sleep(0.25) # Don't try to read more often than 4 times a second.
 
-while True:
-   myMotor.run(Adafruit_MotorHAT.FORWARD)
-   c, d = pi.spi_read(sensorMeat, 2)
-   e, f = pi.spi_read(sensorBot, 2)
-   if c == 2 and e == 2:
-      wordMeat = (d[0]<<8) | d[1]
-      wordBot = (f[0]<<8) | f[1]
-      if (wordMeat & 0x8006) == 0 and (wordBot & 0x8006) == 0: # Bits 15, 2, and 1 should be zero.
-         t = 9*(wordMeat >> 3)/20.0 + 32.0
-         u = 9*(wordBot >> 3)/20.0 + 32.0
-         delta = t - target
-         if(abs(time.time() - timeStat - 10) < 1):
-             timeStat = time.time()
-             if delta < -20:
-                 if motorState == False:
-                     motorState = True
-                     myMotor.setSpeed(100)
-                 else:
-                     motorState == False
-                     myMotor.setSpeed(0)
-             elif delta < 0:
-                 if motorState == False:
-                     motorState = True
-                     myMotor.setSpeed(60)
-                 else:
-                     motorState == False
-                     myMotor.setSpeed(0)
-         print("{:.2f}".format(t) + ' ' + "{:.2f}".format(u) + ' ' + "{:.2f}".format(delta))
-      else:
-         print("bad reading {:b}".format(word))
-   time.sleep(0.25) # Don't try to read more often than 4 times a second.
+
 
 pi.spi_close(sensor)
 
