0faf275730f16ffad9ecee7b09cad98748a08ec4
evargh <varghese.evan@gmail.com>
2018-10-02 19:49:57 -0500

    testing threading

--- GrillHeater.py
+++ GrillHeater.py
@@ -12,16 +12,11 @@ from subprocess import Popen, PIPE
 import logging
 
 temp = StringVar()
-def labelrun():
-    root = Tk()
-    labbel = Label(root, textvariable=temp)
-    labbel.pack()
-    root.mainloop()
-
+temp.set('0')
+root = Tk()
+labbel = Label(root, textvariable=temp)
+labbel.pack()
 panel = threading.Thread(name='panel',target=labelrun())
-panel.setDaemon(True)
-
-panel.start()
 
 
 """
@@ -33,67 +28,62 @@ myMotor.run(Adafruit_MotorHAT.RELEASE);
 """
 pi = pigpio.pi()
 
-class MotorRunner(threading.Thread):
-    def run(self):
-        spi = spidev.SpiDev()
-        spi.open(0, 0)
+spi = spidev.SpiDev()
+spi.open(0, 0)
 
-        spi.max_speed_hz = 500000
-        spi.mode = 0
+spi.max_speed_hz = 500000
+spi.mode = 0
+mh = Adafruit_MotorHAT(addr=0x60)
+def turnOffMotors():
+mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
 
-        mh = Adafruit_MotorHAT(addr=0x60)
-        def turnOffMotors():
-            mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+atexit.register(turnOffMotors)
+myMotor = mh.getMotor(1)
 
-        atexit.register(turnOffMotors)
-        myMotor = mh.getMotor(1)
+if not pi.connected:
+   exit(0)
 
-        if not pi.connected:
-           exit(0)
-
-        sensorMeat = pi.spi_open(0, 1000000, 0) # CE0 on main SPI
-        sensorBot = pi.spi_open(1, 1000000, 0) # CE1 on main SPI
-        timeStat = time.time()
-        target = 100.0
-        motorState = False
-        while True:
-           myMotor.run(Adafruit_MotorHAT.FORWARD)
-           c, d = pi.spi_read(sensorMeat, 2)
-           e, f = pi.spi_read(sensorBot, 2)
-           if c == 2 and e == 2:
-              wordMeat = (d[0]<<8) | d[1]
-              wordBot = (f[0]<<8) | f[1]
-              if (wordMeat & 0x8006) == 0 and (wordBot & 0x8006) == 0: # Bits 15, 2, and 1 should be zero.
-                 t = 9*(wordMeat >> 3)/20.0 + 32.0
-                 u = 9*(wordBot >> 3)/20.0 + 32.0
-                 delta = t - target
-                 if(abs(time.time() - timeStat - 10) < 1):
-                     timeStat = time.time()
-                     if delta < -20:
-                         print('Delta is smaller than -20')
-                         if motorState == False:
-                             motorState = True
-                             myMotor.setSpeed(100)
-                         else:
-                             motorState = False
-                             myMotor.setSpeed(0)
-                     elif delta < 0:
-                         print('Delta is smaller than 0 but not -20')
-                         if motorState == False:
-                             motorState = True
-                             myMotor.setSpeed(60)
-                         else:
-                             motorState = False
-                             myMotor.setSpeed(0)
-                 temp.set("{:.2f}".format(t) + ' ' + "{:.2f}".format(u))
-                 root.update_idletasks()
-                 print("{:.2f}".format(t) + ' ' + "{:.2f}".format(u) + ' ' + "{:.2f}".format(delta) + ' ' + str(motorState))
-              else:
-                 print("bad reading {:b}".format(word))
-           time.sleep(1) # Don't try to read more often than 4 times a second.
-
-x = MotorRunner(name='Motor')
-x.start()
+sensorMeat = pi.spi_open(0, 1000000, 0) # CE0 on main SPI
+sensorBot = pi.spi_open(1, 1000000, 0) # CE1 on main SPI
+timeStat = time.time()
+target = 100.0
+motorState = False
+while True:
+   myMotor.run(Adafruit_MotorHAT.FORWARD)
+   c, d = pi.spi_read(sensorMeat, 2)
+   e, f = pi.spi_read(sensorBot, 2)
+   if c == 2 and e == 2:
+      wordMeat = (d[0]<<8) | d[1]
+      wordBot = (f[0]<<8) | f[1]
+      if (wordMeat & 0x8006) == 0 and (wordBot & 0x8006) == 0: # Bits 15, 2, and 1 should be zero.
+         t = 9*(wordMeat >> 3)/20.0 + 32.0
+         u = 9*(wordBot >> 3)/20.0 + 32.0
+         delta = t - target
+         if(abs(time.time() - timeStat - 10) < 1):
+             timeStat = time.time()
+             if delta < -20:
+                 print('Delta is smaller than -20')
+                 if motorState == False:
+                     motorState = True
+                     myMotor.setSpeed(100)
+                 else:
+                     motorState = False
+                     myMotor.setSpeed(0)
+             elif delta < 0:
+                 print('Delta is smaller than 0 but not -20')
+                 if motorState == False:
+                     motorState = True
+                     myMotor.setSpeed(60)
+                 else:
+                     motorState = False
+                     myMotor.setSpeed(0)
+         temp.set("{:.2f}".format(t) + ' ' + "{:.2f}".format(u))
+         root.update_idletasks()
+         root.update()
+         print("{:.2f}".format(t) + ' ' + "{:.2f}".format(u) + ' ' + "{:.2f}".format(delta) + ' ' + str(motorState))
+      else:
+         print("bad reading {:b}".format(word))
+   time.sleep(1) # Don't try to read more often than 4 times a second.
 
 pi.spi_close(sensorMeat)
 pi.spi_close(sensorBot)
