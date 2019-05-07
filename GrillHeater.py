e58ada7247448c769286cbad7573fd46996d0586
evargh <varghese.evan@gmail.com>
2019-05-07 07:48:51 -0500

    annotated the code better and cleaned up the main file

--- GrillHeater.py
+++ GrillHeater.py
@@ -32,11 +32,13 @@ import logging
 
 app = Flask(__name__)
 
+# Initializes piGPIO, which lets me use the Raspi's GPIO pins more freely
 pi = pigpio.pi()
 
+# Opens up the SPI bus, which I think is necessary for running the motors. Then
+# the motors are initialized
 spi = spidev.SpiDev()
 spi.open(0, 0)
-
 spi.max_speed_hz = 500000
 spi.mode = 0
 mh = Adafruit_MotorHAT(addr=0x60)
@@ -49,23 +51,16 @@ myMotor = mh.getMotor(1)
 if not pi.connected:
    exit(0)
 
-def setTemp(nice):
-    target.set(int(nice))
-
 sensorMeat = pi.spi_open(0, 1000000, 0) # CE0 on main SPI
 sensorBot = pi.spi_open(1, 1000000, 0) # CE1 on main SPI
 motorState = False
 
 meatTemps = {
-    0: 150
+    0: 0
 }
 smokeTemps = {
-    0: 150
+    0: 0
 }
-
-# TODO:  make two arrays, one for meat temperature and the other for the smoker
-# temperature with a limit of 30 indices and store it to JSON. The index page
-# will then be able to read both of them in
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
@@ -83,19 +78,29 @@ if __name__ == '__main__':
 def motorRunner(target = 200, timeState = time.time()):
     int index = 3
     myMotor.run(Adafruit_MotorHAT.FORWARD)
+    # Reading thermocouple values
     c, d = pi.spi_read(sensorMeat, 2)
     e, f = pi.spi_read(sensorBot, 2)
+    # Verifying the validity of all reads
     if c == 2 and e == 2:
         wordMeat = (d[0]<<8) | d[1]
         wordBot = (f[0]<<8) | f[1]
         if (wordMeat & 0x8006) == 0 and (wordBot & 0x8006) == 0:
+            # Converting all information to fahrenheit
             t = 9*(wordMeat >> 3)/20.0 + 32.0
             nu = 9*(wordBot >> 3)/20.0 + 32.0
+            # Adding values to the dictionaries
             meatTemps.update([index, t])
             smokeTemps.update([index, nu])
+            # Establishing the change in temperature that needs to be corrected
             delta = t - target
+            # Checking every ten seconds or so, accounting for long-term variance
             if(abs(time.time() - timeStat - 10) < 1):
                 timeStat = time.time()
+                # Initializes the program to one of two stages: accelerated
+                # change, or slower change. This system runs off of a negative
+                # feedback loop. The fans either pulse at a high speed, or a low
+                # speed
                 if delta < -20:
                     print('Delta is smaller than -20')
                     if motorState == False:
@@ -113,17 +118,19 @@ def motorRunner(target = 200, timeState = time.time()):
                         motorState = False
                         myMotor.setSpeed(0)
              temp.set("{:.2f}".format(t))
-             root.update_idletasks()
-             root.update()
+             # Prints values to conole for testing
              print("{:.2f}".format(t) + ' ' + "{:.2f}".format(u) + ' ' + "{:.2f}".format(delta) + ' ' + str(motorState))
          else:
              print("bad reading {:b}".format(word))
      time.sleep(1)
      index += 3
+     # Dumps the values to a text file, which can later be interpreted by
+     # JS on the index page
      json.dump(meatTemps, "meatTemp.txt")
      json.dump(smokeTemps, "smokeTemp.txt")
-   # Don't read more often than 4 times a second
+   # Don't read more than 4 times a second
 
+# Closes all sensors and files. Best practices
 open('meatTemp.txt', 'w').close()
 open('smokeTemp.txt', 'w').close()
 pi.spi_close(sensorMeat)
