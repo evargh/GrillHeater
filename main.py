5a331ee519df02e0e36ca7a8f0235bb96b40f9dc
evargh <varghese.evan@gmail.com>
2019-05-27 15:49:09 -0500

    finalizing celery things i hope

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ from flask import Flask, redirect, render_template, request, url_for, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length
+from flask_celery import make_celery
 
-from GrillHeater import MotorRunner
-
-from my_app.tasks import MotorRunner
-
+from tasks import MotorRunner
+from celery import Celery, current_task
+from celery.result import AsyncResult
 
 app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
 celery = make_celery(app)
 
+
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    iterations = request.args.get('iterations', 1000)
-    step = 10
+    runner.delay()
+    return render_template('index.html')
 
-    result = MotorRunner.delay_or_fail(
-        target=150
-    )
-    return render_template('index.html', task_id=result.task_id)
+
+@celery.task(name='tasks.MotorRunner')
+def runner():
+    MotorRunner()
+
 
 if __name__ == '__main__':
     app.run()
